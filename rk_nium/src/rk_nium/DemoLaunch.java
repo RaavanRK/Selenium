e52f83b5b8b64ffe399cf8e3989307f9125a6e79
vishal <vishal@DESKTOP-RCQ9CJ3>
2022-03-09 16:58:55 +0530

    1st

---
+++ rk_nium/src/rk_nium/DemoLaunch.java
@@ -0,0 +1,10 @@
+package rk_nium;
+
+public class DemoLaunch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		//webDriver driver=new ChromeDriver();
+		
+	}
+
+}
