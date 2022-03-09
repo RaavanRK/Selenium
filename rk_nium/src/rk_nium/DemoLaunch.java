f89b4974266010ec1b99adcd91f85dd15311c682
vishal <vishal@DESKTOP-RCQ9CJ3>
2022-03-09 21:06:39 +0530

    3rd commit

--- rk_nium/src/rk_nium/DemoLaunch.java
+++ rk_nium/src/rk_nium/DemoLaunch.java
@@ -1,9 +1,13 @@
 package rk_nium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoLaunch {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
-		//webDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
+		//driver.get("https/www.google.com/");
 		
 	}
 
