8e1c69e5c50a18516ad3f13e4c466d209a8ea046
vishal <vishal@DESKTOP-RCQ9CJ3>
2022-03-18 00:09:40 +0530

    google, fb login,enter

--- rk_nium/src/rk_nium/DemoLaunch.java
+++ rk_nium/src/rk_nium/DemoLaunch.java
@@ -1,14 +1,48 @@
 package rk_nium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoLaunch {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		//driver.get("https/www.google.com/");
 		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		//driver.get("www.facebook.com");
+	//	Thread.sleep(2000);
+	//	driver.navigate().back();
+		
+	//	Thread.sleep(2000);
+	//	driver.navigate().forward();
+		
+	//	Thread.sleep(2000);
+	//	driver.navigate().refresh();
+		
+	//	driver.navigate().to("https/www.google.com/");
+		
+		Thread.sleep(2000);
+		WebElement search = driver.findElement(By.name("q"));
+	//	search.sendKeys("java");
+		search.sendKeys("java",Keys.ENTER);
+		
+		driver.navigate().to("https://en-gb.facebook.com/");
+		WebElement email= driver.findElement(By.id("email"));
+		email.sendKeys("pavan kalyan");
+		
+		WebElement pass=driver.findElement(By.name("pass"));
+		pass.sendKeys("hulcut");	
+		
+		WebElement login=driver.findElement(By.name("login"));
+		login.click();
+		
+		Thread.sleep(2000);
+		driver.quit();
 	}
 
 }
